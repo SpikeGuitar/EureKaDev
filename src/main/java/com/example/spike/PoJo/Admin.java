b06b38b7f11f23f7d99e1024f2888cf232d98ac8
2926169741@qq.com <ZYZW656803@w>
2022-04-19 12:00:16 +0800

    Redis ZSet的使用

--- src/main/java/com/example/spike/PoJo/Admin.java
+++ src/main/java/com/example/spike/PoJo/Admin.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class Admin {
     private String name;
     private String msg;
+    private double score;
 }
--- src/main/java/com/example/spike/controller/TestController.java
+++ src/main/java/com/example/spike/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.util.Set;
 
 @RestController
 public class TestController {
@@ -14,17 +15,63 @@ public class TestController {
     @Inject
     private RedisTemplate redisTemplate;
 
-
+    /**
+     * redis获取hash格式数据方法
+     *
+     * @param admin
+     * @return adminResult
+     */
     @PostMapping("/getRedis")
     public Object getRedis(@RequestBody Admin admin){
-        Object adminResult = redisTemplate.opsForHash().get("admin",admin.getName());
+        // key为redis键值
+        String key = "admin";
+        // admin.getName()为hash的key值
+        Object adminResult = redisTemplate.opsForHash().get(key,admin.getName());
         return adminResult;
     }
 
+    /**
+     * redis hash格式存储数据的方法
+     *
+     * @param admin
+     * @return OK
+     */
     @PostMapping("/addRedis")
     public String addRedis(@RequestBody Admin admin){
-        redisTemplate.opsForHash().put("admin",admin.getName(), admin);
+        // key为redis键值
+        String key = "admin";
+        // admin.getName()为hash的key值  admin为存储进redis的数据对象
+        redisTemplate.opsForHash().put(key,admin.getName(), admin);
         return "OK";
     }
 
+    /**
+     * redis 存储ZSet格式数据
+     *
+     * @param admin
+     * @return Ok
+     */
+    @PostMapping("/addRedisZSet")
+    public String addRedisZSet(@RequestBody Admin admin){
+        // key为redis键值
+        String key = "ZSet";
+        // admin为存储的数据  admin.getScore()为ZSet的分值（Score）
+        redisTemplate.opsForZSet().add(key ,admin, admin.getScore());
+        return "OK";
+    }
+
+    /**
+     * 获取redis ZSet的方法
+     *
+     * @param admin
+     * @return adminSet
+     */
+    @PostMapping("/getRedisZSet")
+    public Set<Admin> getRedisZSet(@RequestBody Admin admin){
+        // key为redis键值
+        String key = "ZSet";
+        // 0为开始的分值（score） admin.getScore()为结束的分值
+        Set<Admin> adminSet = redisTemplate.opsForZSet().rangeByScore(key,0,(long)admin.getScore());
+        return adminSet;
+    }
 }
